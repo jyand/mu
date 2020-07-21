c6533cd6e9f7b7c74f49dbee80249a7fb43e2f35
jyand <jyand@protonmail.com>
2020-07-21 07:54:23 +0000

    tokenizer coming along

--- mu.go
+++ mu.go
@@ -6,6 +6,9 @@ import (
         "fmt"
         "os"
         "math"
+//        "encoding/csv"
+//       "encoding/json"
+//        "regexp"
         "strconv"
 )
 
@@ -13,14 +16,22 @@ func feval (f func(float64) float64, x float64) float64 {
         return f(x)
 }
 
+func ftokenizer(op string, x float64) float64 {
+        /*
+        * recursively parse the input function using regular expressions to look for the
+        * op tokens: +,-,*,/,^ switch to returnvalue until string terminates
+        */
+        //return feval()
+}
+
 func main() {
         /* convert commandline syntax to math pkg syntax */
         f := map[string]func(float64) float64 {
                 /* single-variable, real-valued, differentiable functions */
-                "cos": math.Cos,
-                "sin": math.Sin,
-                "e^": math.Exp,
-                "log": math.Log2,
+                "cos": math.Cos, "acos": math.Acos, "cosh": math.Cosh, "acosh": math.Acosh,
+                "sin": math.Cos, "asin": math.Acos, "sinh": math.Cosh, "asinh": math.Acosh,
+                "tan": math.Cos, "atan": math.Acos, "tanh": math.Cosh, "atanh": math.Acosh,
+                "e^": math.Exp, "log": math.Log2, "ln": math.Log,
         }
 
         // testing/debugging
