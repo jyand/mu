9785239374882b1ae9be6c2b61bd020d314e7a91
jyand <jyand@protonmail.com>
2021-02-19 11:51:21 -0500

    sigmoid

--- continuous.go
+++ continuous.go
@@ -8,6 +8,10 @@ func LogBase(b float64, x float64) float64 {
         return math.Log(x)/math.Log(b)
 }
 
+func Sigmoid(x float64) float64 {
+        return (math.Expm1(x) + 1)/(math.Exp(x) + 1)
+}
+
 func Step(x float64) float64 {
         if x < 0 {
                 return 0
