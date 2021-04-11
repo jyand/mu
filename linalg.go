9d3157985859b0fb1c9d3493d27284eb7de1dacf
jyand <jyand@protonmail.com>
2021-04-11 01:34:55 -0400

    csv import, random matrix

--- linalg.go
+++ linalg.go
@@ -1,10 +1,11 @@
 package mu
 
 import (
-        //"math/rand"
+        "math/rand"
+        "time"
         "bufio"
-        "encoding/csv"
-        //"reflect"
+        "os"
+        "strings"
 )
 
 type Vector interface {
@@ -12,12 +13,58 @@ type Vector interface {
 }
 
 type Matrix interface {
-        Dim() (m, n)
+        Dim() (m, n int)
 }
 
-func MatFromCsv(fpath string) mat Matrix {
+// Type switch for a vector
+func VecType(vin Vector) (vout Vector, numeric bool) {
+        switch t := vin.(type) {
+        case int:
+                v := make([]int, len(vin))
+                copy(v, vin)
+                return (v, true)
+        case float64:
+                v := make([]float64, len(vin))
+                copy(v, vin)
+                return (v, true)
+        case uint64:
+                v := make([]uint64, len(vin))
+                copy(v, vin)
+                return (v, true)
+        case complex128:
+                v := make([]int, len(vin))
+                copy(v, vin)
+                return (v, true)
+        default:
+                return (v, false)
+        }
 }
 
-func Decompose(mat *Matrix) vec Vector {
+// defining a matrix by importing data is generally efficient and convenient
+func MatFromCsv(fpath string) (mat Matrix, numeric bool) {
+        file, _ := os.Open(fpath)
+        defer file.Close()
+        var m Matrix
+        sc := bufio.NewScanner(file)
+        for sc.Scan() {
+                m = append(m, (strings.Split(Sc.Text(), ",")))
+        }
+        // apply the type switch on entire matrix as an array of vectors
+        // maybe we don't need both vector and matrix interfaces?
+        return VecType(m)
 }
 
+/* generate an mxn matrix of random, nonnegative real numbers within range (a, b]
+useful for 'weights', Markov Chains, etc. */
+func RandMat(m int, n int, a float64, b float64) mat Matrix {
+        mat := [m][n]float64{}
+        for j := 0 ; j <m ; j++ {
+                for i := 0 ; i < n ; i++ {
+                        rand.Seed(time.Now().UnixNano())
+                        mat[i][j] = b*rand.float64() + a
+                }
+        }
+}
+
+func Transpose(min Matrix) mout Matrix {
+}
